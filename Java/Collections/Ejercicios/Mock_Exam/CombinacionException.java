c8cefdf0916ec1a2badf6d6b522120498b208e1c
Alfonso <114393129+alfon03@users.noreply.github.com>
2023-04-24 23:13:22 +0200

    Add files via upload

---
+++ Java/Collections/Ejercicios/Mock_Exam/CombinacionException.java
@@ -0,0 +1,25 @@
+package Mock_Exam;
+
+public class CombinacionException extends RuntimeException {
+
+	public CombinacionException() {
+	}
+
+	public CombinacionException(String message) {
+		super(message);
+	}
+
+	public CombinacionException(Throwable cause) {
+		super(cause);
+	}
+
+	public CombinacionException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public CombinacionException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
